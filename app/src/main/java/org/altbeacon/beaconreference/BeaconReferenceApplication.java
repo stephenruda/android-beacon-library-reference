de45a703efa50c690956795edf6c66abacfc2568
Stephen Ruda <stephen@simplymadeapps.com>
2019-03-18 16:23:01 -0400

    showing off problem regarding duplicate entry and exit events

--- app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.java
+++ app/src/main/java/org/altbeacon/beaconreference/BeaconReferenceApplication.java
@@ -4,20 +4,16 @@ import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
 import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
+import android.os.RemoteException;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import org.altbeacon.beacon.BeaconManager;
 import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
-import org.altbeacon.beacon.startup.RegionBootstrap;
 import org.altbeacon.beacon.startup.BootstrapNotifier;
+import org.altbeacon.beacon.startup.RegionBootstrap;
 
 /**
  * Created by dyoung on 12/13/13.
@@ -31,16 +27,53 @@ public class BeaconReferenceApplication extends Application implements Bootstrap
 
         beaconManager.setAndroidLScanningDisabled(true);
 
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+
+        // Calling setScanPeriods before starting the service will cause duplicate events
+        // setScanPeriodsToDesiredTimes();
+
+        startForegroundService();
+
+        // Calling setScanPeriods after starting the service will work correctly
+        setScanPeriodsToDesiredTimes();
+
+        Region region = new Region("backgroundRegion", null, null, null);
+        RegionBootstrap regionBootstrap = new RegionBootstrap(this, region);
+    }
+
+    protected void setScanPeriodsToDesiredTimes() {
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
         beaconManager.setBackgroundBetweenScanPeriod(40100l);
         beaconManager.setBackgroundScanPeriod(4100l);
         beaconManager.setRegionExitPeriod(90000l);
         beaconManager.setForegroundBetweenScanPeriod(40100l);
         beaconManager.setForegroundScanPeriod(4100l);
+        try {
+            beaconManager.updateScanPeriods();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
 
-        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24"));
+    protected void startForegroundService() {
+        BeaconManager beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.enableForegroundServiceScanning(getNotificationBuilder().build(), 123);
+        beaconManager.setEnableScheduledScanJobs(false);
+    }
 
-        Region region = new Region("backgroundRegion", null, null, null);
-        RegionBootstrap regionBootstrap = new RegionBootstrap(this, region); }
+    protected void getNotificationChannel() {
+        NotificationChannel channel = new NotificationChannel("notification_channel", "Notification Channel", NotificationManager.IMPORTANCE_DEFAULT);
+        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(channel);
+    }
+
+    public Notification.Builder getNotificationBuilder() {
+        getNotificationChannel();
+        Notification.Builder builder = new Notification.Builder(this, "notification_channel");
+        builder.setSmallIcon(R.drawable.ic_launcher);
+        builder.setContentTitle("Scanning for Beacons");
+        return builder;
+    }
 
     @Override
     public void didEnterRegion(Region region) {
--- app/src/main/java/org/altbeacon/beaconreference/StartServiceActivity.java
+++ app/src/main/java/org/altbeacon/beaconreference/StartServiceActivity.java
@@ -1,59 +1,62 @@
 package org.altbeacon.beaconreference;
 
+import android.Manifest;
 import android.app.Activity;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.v4.app.NotificationCompat;
-import android.view.View;
+import android.util.Log;
 
 public class StartServiceActivity extends Activity {
+
+    private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_stop_service);
-
-        getNotificationChannel();
+        checkPermissions();
     }
 
-    public void pressedButton(View v) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = preferences.edit();
-
-        boolean isUsingForegroundService = preferences.getBoolean("foreground_service_active",false);
-        if(isUsingForegroundService) {
-            editor.putBoolean("foreground_service_active", false);
-            editor.commit();
-
-            Intent intent = new Intent(getApplicationContext(), CustomForegroundService.class);
-            stopService(intent);
-        }
-        else {
-            editor.putBoolean("foreground_service_active", true);
-            editor.commit();
-
-            Intent intent = new Intent(getApplicationContext(), CustomForegroundService.class);
-            startForegroundService(intent);
+    protected void checkPermissions() {
+        if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location Access");
+            builder.setMessage("Please grant location access to detect beacons");
+            builder.setPositiveButton(android.R.string.ok, null);
+            builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                @Override
+                public void onDismiss(DialogInterface dialogInterface) {
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_REQUEST_COARSE_LOCATION);
+                }
+            });
+            builder.show();
         }
     }
 
-    protected void getNotificationChannel() {
-        NotificationChannel channel = new NotificationChannel("notification_channel", "Notification Channel", NotificationManager.IMPORTANCE_DEFAULT);
-        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.createNotificationChannel(channel);
-    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_COARSE_LOCATION: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("PERMISSIONS", "coarse location permission granted");
+                } else {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Functionality limited");
+                    builder.setMessage("Since location access has not been granted, this app will not be able to discover beacons when in the background.");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
 
-    private void sendNotification(String direction) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "notification_channel")
-                .setContentTitle("Beacon Reference Application")
-                .setContentText(direction + " a Beacon region")
-                .setSmallIcon(R.drawable.ic_launcher);
-        NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify((int) System.currentTimeMillis(), builder.build());
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                        }
+
+                    });
+                    builder.show();
+                }
+                return;
+            }
+        }
     }
 }
